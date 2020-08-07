c2bbb22826adbb6205ea6afb805dfdc4b319b4a8
ShivamAyush <52344946+ShivamAyush@users.noreply.github.com>
2020-08-07 18:04:11 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -8,9 +8,9 @@ public class Calc {
 		return a - b;
 	}
   
-  public static int mul(int a , int b) {
-		return a * b;
-	}
+//   public static int mul(int a , int b) {
+// 		return a * b;
+// 	}
   
   public static int div(int a , int b) {
 		return a / b;
@@ -19,7 +19,7 @@ public class Calc {
 	public static void main(String[] args) {
 		System.out.println(add(10,20));
     System.out.println(sub(10,20));
-    System.out.println(mul(10,20));
+    //System.out.println(mul(10,20));
     System.out.println(div(10,20));
 	}
 
